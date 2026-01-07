961eb5ea836fda44d233eda690e4818412aaa30e
Sorin Voinea <sorin.vd@gmail.com>
2026-01-07 12:15:54 +0200

    Initial version as generated by Spring Initializr

---
+++ src/main/java/com/itschool/organdb/OrganDbApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.organdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itschool/organdb/OrganDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.organdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
